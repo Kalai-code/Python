18c054b30426d8be6a87c45e2b6b3eabd113f4c1
Kalaichitra Rangasamy <kalai.workmailbox@gmail.com>
2022-03-29 10:34:28 -0500

    Updated with additional methods

--- SelfLearning/Users.py
+++ SelfLearning/Users.py
@@ -7,6 +7,7 @@ class User:
         self.last_name = last_name
         self.age = age
         self.location = location
+        self.login_attempts = 0
     
     def describe_user(self):
         print("Here is the user information\n")
@@ -16,8 +17,21 @@ class User:
         
     def greet_user(self):
         print(f"Hello, {self.first_name} {self.last_name} !! Welcome to the world of Python")
+    
+    def increment_login_attempts(self):
+        self.login_attempts = self.login_attempts + 1
+        return self.login_attempts
+        
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f"Login attempt has been reset to {self.login_attempts}.")
 
 user = User("David","John",37,"Washington")
 
 user.describe_user()
 user.greet_user()
+print("Login attempt:", user.increment_login_attempts())
+print("Login attempt:", user.increment_login_attempts())
+print("Login attempt:", user.increment_login_attempts())
+user.reset_login_attempts()
+print("Login attempt after reset:", user.increment_login_attempts())
--- SelfLearning/restaurant.py
+++ SelfLearning/restaurant.py
@@ -1,19 +1,30 @@
 class Restaurant:
     """Try it yourself Resturant class"""
     
-    def __init__(self,restuarant_name,cuisine_type):
+    def __init__(self,restuarant_name,cuisine_type,number_seats = 0,number_served = 0):
         """Initialize restuarant_name and cuisine_type attributes"""
         self.restuarant_name = restuarant_name
         self.cuisine_type = cuisine_type
+        self.number_seats = number_seats
+        self.number_served = number_served
     
     def describe_restuarant(self):
         """Print restuarant_name and cuisine_type"""
         print("Name of the restuarant:",self.restuarant_name)
         print("Cuisine Type:",self.cuisine_type)
+        print("No of people can be accomodated in the restuarant:", self.number_seats)
         
     def open_restuarant(self):
         """Print that the restuarant is open"""
         print(f"The restuarant {self.restuarant_name} is open.")
+        
+    def set_number_served(self):
+        """print the number of people server"""
+        print(f"Number of the people served:", self.number_served)
+        
+    def increment_number_served(self):
+        """increment the number of customers served"""
+        print(f"Number of people served today:",self.number_served + 5)
         
 restuarant = Restaurant("Asia Cafe","Chinese")
 
@@ -22,19 +33,25 @@ print(f"Cuisine :", restuarant.cuisine_type)
 
 restuarant.describe_restuarant()
 restuarant.open_restuarant()
+restuarant.set_number_served()
+restuarant.increment_number_served()
 
-restuarant_thai = Restaurant("Thai Lada","Thai")
+restuarant_thai = Restaurant("Thai Lada","Thai",15,10)
 
 print("\nRestaurant Name:",restuarant_thai.restuarant_name)
 print(f"Cuisine :", restuarant_thai.cuisine_type)
 
 restuarant_thai.describe_restuarant()
 restuarant_thai.open_restuarant()
+restuarant_thai.set_number_served()
+restuarant_thai.increment_number_served()
 
-restuarant_italian = Restaurant("Mandola","Italian")
+restuarant_italian = Restaurant("Mandola","Italian",50,20)
 
 print("\nRestaurant Name:",restuarant_italian.restuarant_name)
 print(f"Cuisine :", restuarant_italian.cuisine_type)
 
 restuarant_italian.describe_restuarant()
 restuarant_italian.open_restuarant()
+restuarant_italian.set_number_served()
+restuarant_italian.increment_number_served()
