c6dce2bff1f92d2ae0e108291ed8557226371956
Kalaichitra Rangasamy <kalai.workmailbox@gmail.com>
2022-04-05 11:14:31 -0500

    Changed the code to add class module

--- Projects/LMS/app.py
+++ Projects/LMS/app.py
@@ -1,64 +1,16 @@
 import lms_masterdata
-import lms_engine
+from lms_engine import LMSEngine
 import sys
-"""
-# Read input from user using input
-lms_CustName = input("Enter Customer Name: ")
-lms_CreditScore = input("Enter the Credit Score: ")
-lms_ReqLoanAmt = input("Enter the Loan Amount: ")
 
-# Function call for reading input from  user
-lms_engine.engine(lms_masterdata.lms_masterdata,lms_CustName,int(lms_CreditScore),int(lms_ReqLoanAmt))
-"""
-"""
-# Sys Arg input
-customer_data = sys.argv
-lms_CustName = customer_data[1]
-lms_CreditScore = customer_data[2]
-lms_ReqLoanAmt = customer_data[3]
 
-# Function call for Sys Arg input
-lms_engine.engine(lms_masterdata.lms_masterdata,lms_CustName,int(lms_CreditScore),int(lms_ReqLoanAmt))
-"""
-"""
+
 # Unit Test for one customer
-l_CustName="Alex"
+l_CustName="Ryan"
 l_CustCreditScore=333
 l_CustReqLoanAmount=23500
 
+lms_engine = LMSEngine(lms_masterdata.lms_masterdata,l_CustName,l_CustCreditScore,l_CustReqLoanAmount)
 # Function call for one customer
-lms_engine.engine(lms_masterdata.lms_masterdata,l_CustName,l_CustCreditScore,l_CustReqLoanAmount)
-"""
-# Unit Test for few customers who are eligible and not eligible for the loan
-customers  = [{ "CustName":"Alan"
-  ,"CustCreditScore":200
-  ,"CustReqLoanAmount":23500},
- { "CustName":"Ben"
-  ,"CustCreditScore":400
-  ,"CustReqLoanAmount":23500},
-  { "CustName":"David"
-  ,"CustCreditScore":400
-  ,"CustReqLoanAmount":65000},
-{ "CustName":"Flex"
-  ,"CustCreditScore":100
-  ,"CustReqLoanAmount":23500},
-{ "CustName":"Jan"
-  ,"CustCreditScore":333
-  ,"CustReqLoanAmount":23500}  
-  ]
- 
- # loop through each customer and check if they are eligible for loan or not and return the output
-for c1 in customers:
-    lms_engine.engine(lms_masterdata.lms_masterdata,c1["CustName"],c1["CustCreditScore"],c1["CustReqLoanAmount"])
-    
-"""
-# Unit Test for Customer who is not eligible for loan
+lms_engine.engine()
 
-l_CustName="Adam"
-l_CustCreditScore=90
-l_CustReqLoanAmount=23500
 
-# Function call for Customer who is not eligible for loan
-lms_engine.engine(lms_masterdata.lms_masterdata,l_CustName,l_CustCreditScore,l_CustReqLoanAmount)
-
-"""
--- Projects/LMS/lms_engine.py
+++ Projects/LMS/lms_engine.py
@@ -1,37 +1,31 @@
-
-"""
-def engine(i_masterdata,i_cust_name,i_cust_creditscore,i_cust_loan_amt):
-    #Function to check whether the customer is eligible for the loan or not
-    eligible = False
-    for c in i_masterdata:
-        if i_cust_creditscore>= c["cs_start"] and i_cust_creditscore<= c["cs_end"] and i_cust_loan_amt>=c["loan_amt_start"]and i_cust_loan_amt<c["loan_amt_end"]:
-           eligible = True
-           print(f"\nCongratulations {i_cust_name}!!! You are eligible for the loan")
-           print("Duration for payment is:",c["duration"])
-           print("Your interest rate will be:",c["interest"])   
-           print("The total amount to be paid will be:", i_cust_loan_amt + (i_cust_loan_amt)*(c["interest"])/100)
-
-    if eligible != True:
-        print(f"\n Hi {i_cust_name}, your credit score is too less to provide the loan. \n Please improve the credit score and then apply for the loan. \n Thank you")
-"""       
-# changed the code to write output into a text file      
-def engine(i_masterdata,i_cust_name,i_cust_creditscore,i_cust_loan_amt):
-    """ Function to check whether the customer is eligible for the loan or not """
-    eligible = False
-    for c in i_masterdata:
-        if i_cust_creditscore>= c["cs_start"] and i_cust_creditscore<= c["cs_end"] and i_cust_loan_amt>=c["loan_amt_start"]and i_cust_loan_amt<c["loan_amt_end"]:
-           eligible = True
-           eli_duration = c["duration"]
-           eli_interest = c["interest"]
-           eli_totalamt = i_cust_loan_amt + (i_cust_loan_amt)*(c["interest"])/100
-           fileName = "C:\\PythonLearning\\Projects\\LMS\\Text_Files\\"+i_cust_name+".txt"
-           file_open = open(fileName,'w')
-           file_open.write("\nCongratulations " + i_cust_name+"!!! You are eligible for the loan\n")
-           file_open.write("Duration for payment is: " + str(eli_duration) + "\n")
-           file_open.write("Your interest rate will be: " + str(eli_interest)+ "\n")
-           file_open.write("The total amount to be paid will be: " + str(eli_totalamt) + "\n")
-           file_open.close()
-    if eligible != True:
-        print(f"\n Hi {i_cust_name}, your credit score is too less to provide the loan. \n Please improve the credit score and then apply for the loan. \n Thank you")
+class LMSEngine:
+    """Class to check whether the customer is eligible for the loan or not"""
+    
+    def __init__(self,i_masterdata,i_cust_name,i_cust_creditscore,i_cust_loan_amt):
+        self.i_masterdata = i_masterdata
+        self.i_cust_name = i_cust_name
+        self.i_cust_creditscore = i_cust_creditscore
+        self.i_cust_loan_amt = i_cust_loan_amt
+    
+    # changed the code to write output into a text file      
+    def engine(self):
+        """ Function to check whether the customer is eligible for the loan or not """
+        eligible = False
+        for c in self.i_masterdata:
+            if self.i_cust_creditscore>= c["cs_start"] and self.i_cust_creditscore<= c["cs_end"] and self.i_cust_loan_amt>=c["loan_amt_start"]and self.i_cust_loan_amt<c["loan_amt_end"]:
+               eligible = True
+               eli_duration = c["duration"]
+               eli_interest = c["interest"]
+               eli_totalamt = self.i_cust_loan_amt + (self.i_cust_loan_amt)*(c["interest"])/100
+               fileName = "C:\\PythonLearning\\Projects\\LMS\\Text_Files\\"+self.i_cust_name+".txt"
+               file_open = open(fileName,'w')
+               file_open.write("\nCongratulations " + self.i_cust_name+"!!! You are eligible for the loan\n")
+               file_open.write("Duration for payment is: " + str(eli_duration) + "\n")
+               file_open.write("Your interest rate will be: " + str(eli_interest)+ "\n")
+               file_open.write("The total amount to be paid will be: " + str(eli_totalamt) + "\n")
+               file_open.close()
+               print("The details has been sucessfully entered in a text file")
+        if eligible != True:
+            print(f"\n Hi {self.i_cust_name}, your credit score is too less to provide the loan. \n Please improve the credit score and then apply for the loan. \n Thank you")
         
     
