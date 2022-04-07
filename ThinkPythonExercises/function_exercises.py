30b3bda109208f7095fe24344b1dac8c5381b6da
Kalaichitra Rangasamy <kalai.workmailbox@gmail.com>
2022-04-07 11:49:23 -0500

    updated with more exercises

--- ThinkPythonExercises/function_exercises.py
+++ ThinkPythonExercises/function_exercises.py
@@ -1,3 +1,29 @@
+# Exercise 3-2
+#Function to repeat twice
+
+# passing function object as a parameter to do_twice function
+def do_twice(func):
+    func() # call the function object twice
+    func()
+    
+def print_spam():
+    print("Printing the output twice by passing function object to a function")
+
+# call do_twicw function and pass print_spam function as parameter
+do_twice(print_spam)
+
+# Modified the above function as per the problem
+# passing function object as a parameter and text as value to do_twicewithvalObj function
+def do_twicewithvalObj(func,text):
+    func(text) # call the function object twice
+    func(text)
+    
+def print_spam(print_text):
+    print(print_text)
+
+# call do_twicw function and pass print_spam function as parameter and text as value
+do_twicewithvalObj(print_spam,"Printing the output twice by passing function object and value as parameters to a function")
+
 # Exercise 3-3.
 # write a function to draw a grid
 
